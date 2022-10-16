a65548883db2cb8e5d80b70fb18c4d9999b95c46
Leon-Chun <s1235753@gmail.com>
2022-10-16 18:11:31 +0800

    feat: add express-handlebars

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const PORT = 3000
 
 const app = express()
+
+//handlebars setting 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')   
 
 // user body get 
 app.use(express.urlencoded({ extended: true }))
 
 //router setting
 app.get('/', (req, res) => {
-  res.send('This is my test page for initialization')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
